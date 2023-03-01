76eda303332e92f9e954f3002b3c882651c4d849
Muhammad Zaini <muhammadzaini7475@gmail.com>
2023-03-01 18:48:50 +0800

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about',[
+        "nama" => "Muhammad Zaini",
+        "email" => "muhammadzaini7475@gmail.com",
+
+    ]);
 });
